824477f0509ee6fb794551c13ee285a830e7fe28
Maria Claudia <mariacladiaperezes@gmail.com>
2021-02-03 15:17:37 -0300

    se cambio el for y el id

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -7,33 +7,28 @@
         {{-- es una llave de seguridad cuando recibe la informacion post store, le permite el acceso si no  no podemos guardar la informacion --}}
         {{csrf_field()}}
         <div class="form-group mb-2">
-          <label for="id_autor">Id</label>
-          <input type="number" class="form-control" id="id_autor" aria-describedby="id del autor" placeholder="Id del autor">
+            <label for="title">Título</label>
+            <input type="text" class="form-control" id="title" placeholder="Título del libro">
         </div>
         <div class="form-group mb-2">
-            <label for="titulo_libro">Título</label>
-            <input type="text" class="form-control" id="titulo_libro" placeholder="Título del libro">
-        </div>
-        <div class="form-group mb-2">
-          <label for="nombre_autor">Autor</label>
-          <input type="text" class="form-control" id="nombre_autor" placeholder="Nombre del autor">
+          <label for="author">Autor</label>
+          <input type="text" class="form-control" id="author" placeholder="Nombre del autor">
         </div>
 
         <div class="form-group mb-3">
-            <label for="anio_publicacion">Año</label>
-            <input type="text" class="form-control" id="anio_publicacion" placeholder="Año de Publicación">
+            <label for="year">Año</label>
+            <input type="text" class="form-control" id="year" placeholder="Año de Publicación">
         </div>
 
         <div class="form-group mb-2">
-            <label for="resumen">Resumen</label>
-            <textarea class="form-control" placeholder="Resumen del libro" id="resumen"></textarea>
+            <label for="summary">Resumen</label>
+            <textarea class="form-control" placeholder="Resumen del libro" id="summary" cols="30" rows="10"></textarea>
         </div>
 
-
         <div class="form-group mb-3">
-            <label for="portada">Portada</label>
-            <input type="file" class="form-control" id="portada" placeholder="Portada del libro" accept="image/x-png,image/gif,image/jpeg" />
-            <img id="preview-img" src="" alt="vista previa de la imagen" />
+            <label for="image">Portada</label>
+            <input type="file" class="form-control" id="image" placeholder="Portada del libro" accept="image/x-png,image/gif,image/jpeg" />
+            <img id="preview-img" src="" alt="vista previa de la imagen"/>
         </div>
 
         <button type="submit" class="btn btn-primary">Guardar</button>
