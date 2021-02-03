783d297b9c4d334df1cc5d6ae62f017137cd6167
Maria Claudia <mariacladiaperezes@gmail.com>
2021-02-03 15:07:18 -0300

    se agregaron clases a la tabla

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -2,13 +2,16 @@
 @extends('base')
 @section('title') Inicio @endsection
 @section('content')
-    <table class="table">
-        <thead>
+    <table class="table mt-5">
+        <thead class="tabla-encabezado">
             <tr>
                 <th>{{"Id"}}</th>
-                <th>{{"Titulo"}}</th>
-                <th>{{"Autor"}}</th>
-                <th>{{"Acciones"}}</th>
+                <th class="text-center">{{"Titulo"}}</th>
+                <th class="text-center">{{"Autor"}}</th>
+                <th class="text-center">{{"Año"}}</th>
+                <th class="text-center">{{"Resumen"}}</th>
+                <th class="text-center">{{"Portada"}}</th>
+                <th class="text-center">{{"Acciones"}}</th>
             </tr>
         </thead>
         <tbody>
@@ -18,8 +21,10 @@
                         <td scope="row">{{$post->id}}</td>
                         <td>{{$post->title}}</td>
                         <td>{{$post->author}}</td>
-                        <td>{{$post->author}}</td>
-                        <td><button>Editar</button><button>Eliminar</button></td>
+                        <td>{{$post->year}}</td>
+                        <td>{{$post->summary}}</td>
+                        <td>{{$post->image}}</td>
+                        <td><button class="w-100 mb-3 btn btn-warning">Editar</button><button class="w-100 btn btn-danger">Eliminar</button></td>
                     </tr>
 
                 @endforeach
